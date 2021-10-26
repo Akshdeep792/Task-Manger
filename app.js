122a064f80b24ed8cf5e3844b13dc724d90fb894
Akshdeep792 <76034766+Akshdeep792@users.noreply.github.com>
2021-10-26 15:28:00 -0400

    Mongo update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");  
+const mongoose = require("mongoose");
+ 
 const path = require("path");    
 const app = express();
 let items = [];
@@ -10,15 +11,52 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+mongoose.connect("mongodb//localhost:27017/todoListDb")
+.then(() => console.log("Connection Succesful!......."))
+.catch((err) => console.log(err));
 
 
-// console.log(date());
 
+const itemSchema = new mongoose.Schema(
+    {
+        itemName: {
+            type: String
+        }
+    }
+);
+
+const Item = new mongoose.model("lists", itemSchema);
+
+const groceryList = async () =>{
+    try{
+        const bread = new Item(
+            {
+                itemName : "Bread"
+            }
+        )
+        const biscuits = new Item(
+            {
+                itemName : "Biscuits"
+            }
+        )
+        const softDrink = new Item(
+            {
+                itemName : "Softdrink"
+            }
+        )
+            const result = await Item.insertMany([bread, biscuits, softDrink]);
+            console.log(result);
+    }catch(err){
+        console.log(err);
+    }
+}
+
+groceryList();
 app.get("/", function (req, res) {
-    // res.sendFile(__dirname + "index.html");
+  
     
-    let day = date.getDate();
-    res.render("list", { listTitle: day , newListItems: items});
+    
+    res.render("list", { listTitle: "Today" , newListItems: items});
 });
 
 app.post("/", function(req, res){
@@ -40,12 +78,7 @@ app.get("/work", function(req, res){
    res.render("list", {listTitle: "Work List", newListItems: workItem});
 });
 
-// app.post("/work", function(req , res){
-//     let item = req.body.newItem;
 
-//     workItem.push(item);
-//     res.redirect("/work");
-// })
 
 app.listen(3000, function () {
     console.log("Server started on the port 3000");
